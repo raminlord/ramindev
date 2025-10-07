ef218e22598b62ed460da7689ee52246cafceffb
raminlord GitHub <reslami1360@gmail.com>
2025-10-07 22:35:28 +0330

    case study-ready404/07/15

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+    // یا در نسخه‌های جدیدتر:
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig;
 
 export default nextConfig;
